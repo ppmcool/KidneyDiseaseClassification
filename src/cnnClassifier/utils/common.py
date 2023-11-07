7fe15f9f529c06ae78d6647325282287d6cbe4c1
ppmcool <pritampatra988@gmail.com>
2023-11-07 14:31:29 +0530

    utility code added in Common.py

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,2 +1,21 @@
 import os
-from box.
+from box.exceptions import BoxTypeError
+from ensure import ensure_annotations
+from box import ConfigBox
+import sys
+import json
+from pathlib import Path
+from cnnClassifier import logger
+import base64
+import yaml
+
+def load_json(path:Path) ->ConfigBox:
+
+    try:
+        with open(path) as f:
+            content=json.load(f)
+        logger.info(f"json file loaded successfully from:{path}")
+    except Exception as e:
+        logger.info(e)
+        raise e
+    
